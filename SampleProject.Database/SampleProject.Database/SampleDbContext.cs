1836758341d7f45085b3612632458e694949130a
charlesflorido <charlesflorido@outlook.com>
2023-02-20 10:14:20 +0800

    initialize database project

---
+++ SampleProject.Database/SampleProject.Database/SampleDbContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace SampleProject.Database
+{
+    public partial class SampleDbContext : DbContext
+    {
+        public SampleDbContext()
+        {
+        }
+
+        public SampleDbContext(DbContextOptions<SampleDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Id).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.CreatedTimestamp).HasDefaultValueSql("(getutcdate())");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ SampleProject.Database/SampleProject.Database/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleProject.Database
+{
+    public partial class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [StringLength(256)]
+        public string FirstName { get; set; } = null!;
+        [StringLength(256)]
+        public string LastName { get; set; } = null!;
+        [StringLength(256)]
+        [Unicode(false)]
+        public string Email { get; set; } = null!;
+        [StringLength(256)]
+        public string? AddressLine { get; set; }
+        [StringLength(128)]
+        public string? City { get; set; }
+        [StringLength(128)]
+        public string? State { get; set; }
+        [StringLength(16)]
+        public string? Zip { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? DateOfBirth { get; set; }
+        public int? CreditScore { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime CreatedTimestamp { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? UpdatedTimestamp { get; set; }
+    }
+}
